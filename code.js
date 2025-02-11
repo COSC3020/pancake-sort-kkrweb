7020ea0d460dcf8240cf8ded865dc8c426ef523d
Kane Kriz <kanemkriz@gmail.com>
2025-02-10 18:25:35 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // Kane Kriz
 // UWYO COSC 3020
-// 4 Feb 2025
+// 10 Feb 2025
 //
 //
 
@@ -8,15 +8,15 @@
 function flip(array, n)
 {
 var arrLen = array.length;
-var tmpArr = []; //probably not needed
+//var tmpArr = []; //probably not needed
 var flippedArr = [];
-var tmpNum; //probably not needed
+//var tmpNum; //probably not needed
 
 if(n<=1)
 {
     return array;
 }
-else if(n >= arrLen)
+else if(n>=arrLen)
 {
     /*
     for(var i=0, i<(arrLen - 1), i++)
@@ -45,12 +45,45 @@ return flippedArr;
 }
 
 
-// Use only flip() here to manipulate the array
-function pancakeSort(arr)
+// Use only flip() here to manipulate the array - Note to Self, dont change this comment as it was here in the original file template
+// also a good idea to keep it so I don't just forget it after not looking at this file for a while
+function pancakeSort(array)
 {
-    var arrLen0 = arr.length;
-   //nuking this
+    var arrLen0 = array.length;
+    
+    for(var x = arrLen0-1; x>=1; x--)//loop iterates with number of indices in the array as the counter, x
+    {
+        
+        var endElement = array[0]; //will this break if the array is empty []? Probably
+        var endPos = 0;
+        //needs both of the above vars or could be done with just one or the other? Something to think about
+        for(var i=0; i<=x; i++)
+        {
+            if (array[i] > endElement)
+            {
+                endElement = array[i];
+                endPos = i;
+            }
+        }
 
-    return arr;
+        //maximum element to the end of the current array, properly following pancakeSort as intended
+        if(endPos != 0)
+        {
+            array = flip(array, endPos + 1);
+        }
+        array = flip(array, x);
+    }
 
+    return array;
 }
+
+
+
+/*
+//tests
+console.log(pancakeSort([100, 50, 25, 75, 10])); 
+console.log(pancakeSort([8, 3, 6, 2, 9])); 
+console.log(pancakeSort([1])); 
+console.log(pancakeSort([2, 1]));
+console.log(pancakeSort([]));
+*/
